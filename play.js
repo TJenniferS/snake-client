0f70b9bc1e4c3c115b1b6541efc7e888e9faefe6
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-20 19:13:15 -0400

    remove connect function to add to client with export and require connect function from client. snake works the same

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "172.27.131.43", // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // event handler for receiving data from server
-  conn.on("data", (data) => {
-    console.log("received:", data); // log received data
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
