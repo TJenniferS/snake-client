fc40c426a176e80d390c584475513d75323080e5
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-20 20:56:06 -0400

    remove setupInput and handleUserInput to require later

--- play.js
+++ play.js
@@ -4,28 +4,3 @@ const { connect } = require("./client"); // require connect function which conta
 console.log("Connecting ..."); // initial print
 connect(); // then actually call `connect` to connect with game server
 
-
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  // Event listener for user input
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-// handle user input from stdin
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    // Terminate game upon Ctrl + C
-    process.exit();
-  }
-
-};
-
-setupInput();
