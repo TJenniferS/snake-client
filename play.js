5ab467af23dffbf68b2893f5d5ecc70846cce420
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-20 21:04:07 -0400

    require setupInput and call setupInput. game exists with ctrl + c but warning sound from terminal

--- play.js
+++ play.js
@@ -1,6 +1,9 @@
 const { connect } = require("./client"); // require connect function which contains an object in ES6 style from client.js
 
+const { setupInput } = require("./input"); // require setupInput
+
 
 console.log("Connecting ..."); // initial print
 connect(); // then actually call `connect` to connect with game server
 
+setupInput(); // call setupInput
