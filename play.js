ddf7f9dcfee577f2ae51d2519ef32cb5851237e6
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-20 20:34:04 -0400

    add setup Interface to handle user from stdin code exactly as provided

--- play.js
+++ play.js
@@ -1,4 +1,14 @@
 const { connect } = require("./client"); // require connect function which contains an object in ES6 style from client.js
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
 console.log("Connecting ..."); // initial print
 connect(); // then actually call `connect` to connect with game server
