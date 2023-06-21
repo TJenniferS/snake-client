38dca0de051c5d2d0cb179f5316496f778e916cb
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-20 20:49:52 -0400

    fix syntax for setupInput to listen to user input using stdin, create function handleUserInput register as data callback handler for stdin, add process exit function with ctrl + c, call setupInput. without the function, game does not exit with ctrl + c

--- play.js
+++ play.js
@@ -1,5 +1,10 @@
 const { connect } = require("./client"); // require connect function which contains an object in ES6 style from client.js
 
+
+console.log("Connecting ..."); // initial print
+connect(); // then actually call `connect` to connect with game server
+
+
 // setup interface to handle user input from stdin
 
 const setupInput = function() {
@@ -7,8 +12,20 @@ const setupInput = function() {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
+  // Event listener for user input
+  stdin.on("data", handleUserInput);
+
   return stdin;
 };
 
-console.log("Connecting ..."); // initial print
-connect(); // then actually call `connect` to connect with game server
+// handle user input from stdin
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    // Terminate game upon Ctrl + C
+    process.exit();
+  }
+
+};
+
+setupInput();
