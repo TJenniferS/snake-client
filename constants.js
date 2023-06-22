0257d135e4580829e102a5ab25c6fd75caf2a737
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-21 20:37:30 -0400

    revert hash with sensitive info

--- constants.js
+++ constants.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 const IP = "localhost"; // 172.27.131.43 to connect to game server locally
+=======
+const IP = "172.27.131.43";
+>>>>>>> parent of f651367... change IP from 172.27.131.43 to localhost
 const PORT = 50541;
 
 module.exports = {
